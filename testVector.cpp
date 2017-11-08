e62f743576bbd221f4032292ad79279b96d49139
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2017-11-08 00:30:42 -0800

    2017-11-08. Vector test end(), front(), back()

--- testVector.cpp
+++ testVector.cpp
@@ -28,6 +28,48 @@ int main(int argc, char *argv[]){
 	std::cout<<std::endl;
 	std::vector<int> b(a);
 	a.insert(a.begin(),b.rbegin(),b.rbegin()+3);
-	for (int i=0;i<a.size();++i)
-	std::cout << a[i] << " ";
+	for (int i=0;i<a.size();++i){
+		std::cout << a[i] << " ";
+	}
+	std::cout << std::endl << *(a.end());	
+
+
+int width = 20; 
+int width_m = 24;
+int height = 15;
+int vRangeH = 4;
+int vRangeV = 4;
+int snake = 1;
+
+	std::vector<float> xData((size_t)width), yData((size_t)height);
+	std::iota(xData.begin(), xData.end(), 0.0);
+	std::iota(yData.begin(), yData.end(), 0.0);
+	float scaleX = xData.back();
+	float scaleY = yData.back();	// scaleY is range in vertical direction
+	std::for_each(xData.begin(), xData.end(), [scaleX](float& v){v /= scaleX; });//0->100% of scan size
+	std::for_each(yData.begin(), yData.end(), [scaleY](float& v){v /= scaleY; });//0->100% of scan size
+	scaleX = xData.back() / 2;
+	std::for_each(xData.begin(), xData.end(), [scaleX](float& v){v -= scaleX; });//make symmetric about 0
+	scaleY = yData.back() / 2;
+	std::for_each(yData.begin(), yData.end(), [scaleY](float& v){v -= scaleY; });//make symmetric about 0
+	scaleX = 2.0 * vRangeH;
+	scaleY = 2.0 * vRangeV;
+	std::for_each(xData.begin(), xData.end(), [scaleX](float& v){v *= scaleX; });//scale so limits are +/- vRangeH
+	std::for_each(yData.begin(), yData.end(), [scaleY](float& v){v *= scaleY; });//scale so limits are +/- vRangeV
+
+	float d1 = xData[1] - xData[0];
+	// If snake, insert at begin&end.  If raster, only insert at begin.
+	for (int i = 0; i < (width_m - width) / 2; ++i){
+		xData.insert(xData.begin(), xData.front() - d1);
+		if (snake){
+			xData.insert(xData.end(), xData.back() + d1);
+		}
+		else{
+			xData.insert(xData.begin(), xData.front() - d1);
+		}
+	}
+		
+	for (int i=0;i<xData.size();++i){
+		std::cout << xData[i] << " ";
+	}
 }
