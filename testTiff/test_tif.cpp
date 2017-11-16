321006549fac8b6dbb04f8fe5b9a98ccfa2fe683
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2017-11-16 10:38:11 -0800

    2017-11-16 Added a test image.  3 pages, each shift 1 pixel to the right.  Test the fftw correction code.

--- testTiff/test_tif.cpp
+++ testTiff/test_tif.cpp
@@ -7,30 +7,7 @@
 
 int main(int argc, char *argv[]){
 
-	// first make a tif image.
-	std::string fileNameOut_1 = "d:\\output_1.tif";
-	std::string fileNameOut_2 = "d:\\output_2.tif";
-	std::string fileName = "d:\\2by2tif.tif";
-	// std::vector<uint16_t> vec(4);
-	// vec[0] = 0;
-	// vec[1] = pow(2,14)-1;
-	// vec[2] = pow(2,15)-1;
-	// vec[3] = pow(2,16)-1;
-	// Tif::Write(vec.data(),2,2,fileName);		
-	
-	fileName = "d:\\mm.tif";
-	fileName = "d:\\8_stack.tif";
-	// fileName = "d:\\8_bit_grayscale_pc.tif";
-	// fileName = "d:\\16_bit_grayscale_pc.tif";
-	// fileName = "d:\\32_bit_grayscale_pc.tif";
-	// fileName = "d:\\8_bit_rgb_pc.tif";
-	// fileName = "d:\\16_bit_rgb_pc.tif";
-	// fileName = "d:\\32_bit_rgb_pc.tif";
-	// fileName = "d:\\8_bit_rgb_planar_pc.tif";
-	// fileName = "d:\\16_bit_rgb_planar_pc.tif";
-	// fileName = "d:\\32_bit_rgb_planar_pc.tif";
-	// fileName = "d:\\rgbrgb.tif";
-	// fileName = "d:\\rrggbb.tif";
+	std::string fileName = "..\\3stack_small.tif";
 
 	if(argc>1){
 		std::string s(argv[1]);
@@ -41,11 +18,10 @@ int main(int argc, char *argv[]){
 	std::vector<std::vector<uint16_t>> imageFrames;
 	Tif::Read(imageFrames, imageWidth, imageHeight, imageSampleFormat, nChannels, fileName);
 
-	int nF = 0;
-	Tif::Write(imageFrames[2*nF],imageWidth[2*nF]*nChannels[2*nF],imageHeight[2*nF],fileNameOut_1);
-	Tif::Write(imageFrames[2*nF+1],imageWidth[2*nF],imageHeight[2*nF]*nChannels[2*nF],fileNameOut_2);
-
 	std::vector<float> shifts = correlateRows<float>(imageFrames, imageHeight[0], imageWidth[0], false, 100);
+
+	std::cout << "\nshifts of each rows:\n";
+	printVector(shifts);
 
 }
 
