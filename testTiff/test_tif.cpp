2a0e95b8ec940a3316c5b0052d79b36f888efc9d
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2017-11-16 16:20:40 -0800

    2017-11-16.  OK to work with images taken during test. Images were raw before fftw correction.  So we can reapply fftw correction and see how must shift we applied.

--- testTiff/test_tif.cpp
+++ testTiff/test_tif.cpp
@@ -7,7 +7,17 @@
 
 int main(int argc, char *argv[]){
 
-	std::string fileName = "..\\3stack_small.tif";
+	std::string folderName = "C:\\Users\\ZheChen\\Desktop\\Demonstrate Exteranl Shift\\";
+	std::string outputName = folderName + "shift_Log.txt";
+
+	bool snakeTF = false;
+	std::string fileName; 
+	fileName = folderName + "raster_2k_16_frame.tif";	snakeTF = false;
+	fileName = folderName + "raster_4k_8_frame.tif";	snakeTF = false;
+	fileName = folderName + "snake_2k_16_frame.tif";	snakeTF = true;
+	fileName = folderName + "snake_4k_16_frame.tif";	snakeTF = true;
+
+	// fileName = folderName + "3stack_small.tif";
 
 	if(argc>1){
 		std::string s(argv[1]);
@@ -18,10 +28,20 @@ int main(int argc, char *argv[]){
 	std::vector<std::vector<uint16_t>> imageFrames;
 	Tif::Read(imageFrames, imageWidth, imageHeight, imageSampleFormat, nChannels, fileName);
 
-	std::vector<float> shifts = correlateRows<float>(imageFrames, imageHeight[0], imageWidth[0], false, 100);
+	std::vector<float> shifts = correlateRows<float>(imageFrames, imageHeight[0], imageWidth[0], snakeTF, 10);
 
-	std::cout << "\nshifts of each rows:\n";
-	printVector(shifts);
+	std::fstream os;
+	os.open(outputName, std::ios::app | std::ios::binary);	// append
 
+	int ind = 0;
+	for(int iF=0; iF<imageFrames.size(); iF++){
+		os << shifts[ind];
+		ind++;
+		if(0 == iF%2) os << "\t";
+		else os << "\n";
+	}
+	os << "\n";
+
+	os.close();
 }
 
